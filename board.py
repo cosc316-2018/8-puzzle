366d546ea5e86d76ab7eaacc441cfcc3f0e120a4
Aaron George <angeorge200@gmail.com>
2018-10-25 15:25:33 -0400

    Worked primarily on manhattan distance calculation (in group)

--- board.py
+++ board.py
@@ -1,8 +1,8 @@
 class BoardState:
-    # i definitely can't find the banter section in PEP8
-    def __init__(self, q):
+    def __init__(self):
         # currently all 0
-        self.board = [q.items[i: i + 3] for i in range(0, len(q.items), 3)]
+        # self.board = [q.items[i: i + 3] for i in range(0, len(q.items), 3)]
+        self.board = [[5, 8, 1], [2, 0, 6], [3, 7, 4]]
         # should automatically know what the next boards are
         # be careful tho bc it can freak out at the end when it runs out of options
         # THIS IS AN IMPORTANT METHOD WE JUST DONT KNOW HOW TO USE IT YET
@@ -13,11 +13,46 @@ class BoardState:
 
     # most 4 options, least 2
 
-    def show(self):
+    def show_board(self):
         print(self.board)
 
+    def find_position(self, value, x, y):
+        '''Another helper function for manhattan_distance().
+        Finds where the number currently is.'''
+
+        perfect_board = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        perfect_row = 0
+        perfect_col = 0
+
+        for row in range(len(perfect_board)):
+            try:
+                item_location = perfect_board[row].index(item)
+                #checks for WHERE the number 6 is
+            except:
+                item_location = None
+                #if 6 isnt there, return none
+            if item_location:
+                print(row, item_location)
+                perfect_row = row
+                perfect_col = item_location
+                break
+
+        return abs(x - perfect_row) + abs(y - perfect_col)
+
     def manhattan_distance(self):
-        pass
+        '''Calculates the total manhattan distance for a board -
+        finding the distance between each value and where it should be,
+        and adding up the resulting distance values.'''
+        manhattans = 0
+        for a in range(9):
+            for x in range(3):
+                for y in range(3):
+                    if self.board[x][y] == a:
+                        manhattans += self.find_position(a, x, y)
+                        
+        return manhattans
+
+
 
     def check_duplicate(self):
         # see if we've seen this board before
--- main.py
+++ main.py
@@ -6,19 +6,16 @@ q = Queue()
 # this instantiates a queue object as q
 
 q.show()
-# this shows the queue instantiation q
-for i in range(9):
-    q.enqueue(i + 1)
-# this is beyond my mortal understanding
-
-q.show()
 # this shows the queue instantiation, q
 
-b = BoardState(q)
+l=list([1,2,3,4,5,6,7,8,0])
+
+b = BoardState()
 # this is a board state that takes a queue from 1-10 as arguments, which shockingly makes no sense
 
-
-b.show()
+b.show_board()
+print(b.manhattan_distance())
+# b.find_position(1)
 
 # handler
 # use boardstate stuff to
