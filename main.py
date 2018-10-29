7512a4c708284d5b5f118048b00c74b40ecbd209
Aaron George <angeorge200@gmail.com>
2018-10-28 23:57:04 -0400

    Changed some variable names, added a method to generate a random board

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from solve import solve_board
+from random import randint
 
+
+def generate_board():
+    stack = [x for x in range(9)]
+    puzzle = [[stack.pop(randint(0, len(stack) - 1)) for x in range(3)] for y in range(3)]
+    return puzzle
+
+    
 # Boards 0 and 4 are not possible. Boards 1, 2, 3 and 5 are possible.
 my_board0 = [
        [7,4,8],
@@ -38,9 +46,11 @@ my_board4 = [
         ]
 
 #Test cases
-solve_board(my_board0)
-solve_board(my_board1)
-solve_board(my_board2)
-solve_board(my_board3)
-solve_board(my_board4)
-solve_board(my_board5)
+# solve_board(my_board0)
+# solve_board(my_board1)
+# solve_board(my_board2)
+# solve_board(my_board3)
+# solve_board(my_board4)
+# solve_board(my_board5)
+
+solve_board(generate_board())
--- solve.py
+++ solve.py
@@ -8,30 +8,30 @@ def swap(board,x,y,x1,y1):
     board1[x][y], board1[x1][y1] = board1[x1][y1], board1[x][y]
     return BoardState(board1, board)
 
-def make_kids(b):
-    '''Creates the "kid" boards - all of the next possible boards from the
+def create_children(brd):
+    '''Creates the descendants of this board - all of the next possible boards from the
     current position. '''
-    board = b.board
-    kids = []
+    board = brd.board
+    children = []
     for x in range(3):
         for y in range(3):
             if board[x][y] == 0:
                 if x == 0:
-                    kids.append(swap(b, x, y, x+1, y))
+                    children.append(swap(brd, x, y, x+1, y))
                 if x == 1:
-                    kids.append(swap(b, x, y, x - 1, y))
-                    kids.append(swap(b, x, y, x + 1, y))
+                    children.append(swap(brd, x, y, x - 1, y))
+                    children.append(swap(brd, x, y, x + 1, y))
                 if x == 2:
-                    kids.append(swap(b, x, y, x - 1, y))
+                    children.append(swap(brd, x, y, x - 1, y))
                 if y == 0:
-                    kids.append(swap(b, x, y, x, y + 1))
+                    children.append(swap(brd, x, y, x, y + 1))
                 if y == 1:
-                    kids.append(swap(b, x, y, x, y + 1))
-                    kids.append(swap(b, x, y, x, y - 1))
+                    children.append(swap(brd, x, y, x, y + 1))
+                    children.append(swap(brd, x, y, x, y - 1))
                 if y == 2:
-                    kids.append(swap(b, x, y, x, y - 1))
+                    children.append(swap(brd, x, y, x, y - 1))
                 break
-    return kids
+    return children
 
 def is_possible(board):
     '''Checks if the board is possible to be solved. Returns a boolean.'''
@@ -39,10 +39,13 @@ def is_possible(board):
     a = [board[x][y] for x in range(3) for y in range(3)]
     b = [board[x][y] for x in range(3) for y in range(3)]
 
+    # a check for whether any invalid values exist in the board
     b.sort()
     for x in range(9):
         if b[x] != x:
+            print('Invalid value found in board')
             return False
+
     a.remove(0)
     for x in a:
         f = a.index(x)
@@ -50,6 +53,8 @@ def is_possible(board):
         for y in g:
             if y > x:
                 inversions += 1
+    
+    print('Number of Inversions:', inversions)
     if inversions % 2 == 0:
         return True
     else:
@@ -57,27 +62,28 @@ def is_possible(board):
 
 def solve_board(board):
     '''Checks the current board and compares it to the perfect board.
-    It then runs a while loop and the "make_kids" until the current board matches the goal board.'''
+    It then runs a while loop and the "create_children" until the current board matches the goal board.'''
     if not is_possible(board):
         print("Bro, not cool. Enter a valid board next time.")
         return
     q = Queue()
     q.put(BoardState(board, None))
+
     goal_state = [
        [1,2,3],
         [4,5,6],
         [7,8,0],
         ]
-    done = False
+
     visited = Queue()
-    while not done:
+    while True:
         current = q.push()
         # print(current.board)
         visited.put(current)
         if current.board == goal_state:
             print(current.board)
             return
-        a = make_kids(current)
+        a = create_children(current)
         for x in a:
             if q.instances(x) == 0 and visited.instances(x) == 0:
                 q.put(x)
