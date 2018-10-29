b5ad216fc2af8ade6dde849e2017837084ae2fda
Aaron George <angeorge200@gmail.com>
2018-10-29 00:14:48 -0400

    Changed variable names in the generate_board method so that it is more intuitive.

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from random import randint
 
 
 def generate_board():
-    stack = [x for x in range(9)]
-    puzzle = [[stack.pop(randint(0, len(stack) - 1)) for x in range(3)] for y in range(3)]
+    '''Generate a random puzzle to be solved by the 8-puzzle solving algorithm'''
+    num_list = [x for x in range(9)]
+    # adds a random value from the list of numbers to the puzzle
+    #and then removes that value from the list
+    puzzle = [[num_list.pop(randint(0, len(num_list) - 1)) for x in range(3)] for y in range(3)]
     return puzzle
-
 
 
 solve_board(generate_board())
